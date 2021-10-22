43e5f17cd14913367fdafb4754d5e62f2f15ff76
kevinuyeh <isrealkevin929@gmail.com>
2021-10-22 13:47:52 -0800

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots =[
+    {
+        id:1,
+        name: 'Jordan Steve',
+        username:'Bret',
+        email:'jordan@rules.net'  
+    },  
+    {
+        id:2,
+        name: 'Weka Victor',
+        username:'Victor',
+        email:'weka@jones.net'  
+    }, 
+    {
+        id:3,
+        name: 'Kelvin Nou',
+        username:'Kevans',
+        email:'kelvin@madshots.net'  
+    }, 
+    {
+        id:4,
+        name: 'Terdoo Stuffy',
+        username:'Rylo',
+        email:'Tboy@cod.net'  
+    }, 
+    {
+        id:5,
+        name: 'Patrick Starr',
+        username:'Starboy',
+        email:'Pat@ride.net'  
+    },  
+    {
+        id:6,
+        name: 'Benita Rouse',
+        username:'Benny',
+        email:'Bennycheecks@kinka.net'  
+    }, 
+    {
+        id:7,
+        name: 'Francis Ojay',
+        username:'Ojay',
+        email:'francis@koporke.net'  
+    }, 
+    {
+        id:8,
+        name: 'Noah Bokkos',
+        username:'S4YG3',
+        email:'Noah@grime.net'  
+    }, 
+    {
+        id:9,
+        name: 'Tega Oviry',
+        username:'TegaMillie',
+        email:'tega@candypaint.net'  
+    }, 
+    {
+        id:10,
+        name: 'Daniel Akomolafe',
+        username:'Alte Boy',
+        email:'danny@altenuts.net'  
+    }, 
+]
